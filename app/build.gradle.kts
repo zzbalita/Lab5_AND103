107389e5f183da6e89366a3297b737af5f60a54e
zzbalita <zzbalitaba@gmail.com>
2024-08-05 12:00:03 +0700

    minor change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.5.0")
     implementation ("com.google.code.gson:gson:2.10.1")
     implementation ("com.google.code.gson:gson:2.10.1")
-    implementation ("com.squareup.retrofit2:adapter-rxjava2:latest.version")
     implementation ("com.squareup.retrofit2:retrofit:2.5.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.5.0")
     implementation ("com.android.volley:volley:1.2.1")
